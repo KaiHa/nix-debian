c85ccf8c957194c75b49d6fc0ce5b4b219f72401
Kai Harries <kai.harries@gmail.com>
2017-09-17 20:32:35 +0200

    Add dh_make template

---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(nix):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="nix" command="/usr/bin/nix"
---
+++ debian/nix.default.ex
@@ -0,0 +1,10 @@
+# Defaults for nix initscript
+# sourced by /etc/init.d/nix
+# installed at /etc/default/nix by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
