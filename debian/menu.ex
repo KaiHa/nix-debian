882bd6b7c9f9605417728f930963ad767632ed73
Kai Harries <kai.harries@gmail.com>
2017-09-19 19:58:09 +0200

    Adapt template

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(nix):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="nix" command="/usr/bin/nix"
--- debian/nix.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the nix package
-#
-0 4	* * *	root	[ -x /usr/bin/nix_maintenance ] && /usr/bin/nix_maintenance
